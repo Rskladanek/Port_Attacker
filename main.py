5b387226200b7c79b31e836a02891c2522dff756
Rskladanek <remek.skladanek@wp.pl>
2024-12-09 21:10:50 +0100

    improved main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from menu import Menu
 
 
+def main():
+    try:
+        m = Menu()
+        m.run()
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user. Exiting.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")       
+
+
 if __name__ == "__main__":
-    m = Menu()
-    m.run()
+    main()
